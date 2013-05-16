ee3e090752d405c34456e1328508a6444c87a141
Omar <omar@omar-PC.(none)>
2013-05-16 12:15:30 -0600

    Ahorcado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahorcado::Application.config.secret_token = '304be5b212edf73bc4ad1a2082b0613df04058a447de5ea2763761fe91257ca9044b2d6a03ce8cd2b91d1ccdfbd1cefca5cf6b1bdb6e88d79feea1e7c0abdf9a'
